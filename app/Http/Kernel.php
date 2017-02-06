63ea37e69633cf38bb23429898ca434802e7e175
Fèlix Casanellas <felix.casanellas@gmail.com>
2017-02-06 15:10:08 +0100

    LogAfterRequest middleware added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,6 +29,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\LogAfterRequest::class
             //\App\Http\Middleware\VerifyCsrfToken::class,
         ],
 
